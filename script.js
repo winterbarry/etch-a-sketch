140136d8942a14c4c13a81c470766872c3d82b32
Obe Ajilima <ajilimaobe@gmail.com>
2024-06-01 16:58:28 +0100

    added function to create grid of squares

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let container = document.getElementById("container");
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 252; i++) {
     let newDiv = document.createElement('div');
+    newDiv.classList.add('square');  
     container.appendChild(newDiv);
 }
 
